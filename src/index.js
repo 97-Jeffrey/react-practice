be35458e359edd6342b741be5cb27ea76fb45768
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-20 21:14:53 -0800

    App and NavBar component are created and App component is the root component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 import Counters from './components/counters';
 
 ReactDOM.render(
-  <Counters/>,
+  <App/>,
   document.getElementById('root')
 );
 
